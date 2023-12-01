ad501ba2b6e0e21c0b1f1815ca6a9179cdfcd0d5
Mårten Blankfors <marten@blankfors.se>
2023-12-01 14:58:28 +0100

    feat: Naive attempt at day 1 part 2 failed

--- src/day1.rs
+++ src/day1.rs
@@ -4,6 +4,8 @@ pub fn p1(input: &str) -> String {
         .map(|line| line.chars().filter_map(|c| c.to_digit(10)).collect())
         .collect();
 
+    println!("Digits: {:?}", digits);
+
     let sum: u32 = digits
         .into_iter()
         .map(|line_digits| combine_first_and_last_digit(&line_digits))
@@ -13,7 +15,7 @@ pub fn p1(input: &str) -> String {
 }
 
 pub fn p2(input: &str) -> String {
-    todo!();
+    p1(&replace_spelled_out_digits(input))
 }
 
 fn combine_first_and_last_digit(line_digits: &[u32]) -> u32 {
@@ -21,6 +23,19 @@ fn combine_first_and_last_digit(line_digits: &[u32]) -> u32 {
     let last = line_digits.last().expect("No last digit");
 
     first * 10 + last
+}
+
+fn replace_spelled_out_digits(input: &str) -> String {
+    input
+        .replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9")
 }
 
 use crate::solution::Solution;
