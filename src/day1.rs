0a34e40702b8650c74bf730bfacbbd16530656c4
Mårten Blankfors <marten@blankfors.se>
2023-12-01 14:48:23 +0100

    feat: Day 1 part 1 completed

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,26 @@
 pub fn p1(input: &str) -> String {
-    todo!();
+    let digits: Vec<Vec<u32>> = input
+        .split_whitespace()
+        .map(|line| line.chars().filter_map(|c| c.to_digit(10)).collect())
+        .collect();
+
+    let sum: u32 = digits
+        .into_iter()
+        .map(|line_digits| combine_first_and_last_digit(&line_digits))
+        .sum();
+
+    format!("Sum: {}", sum)
 }
 
 pub fn p2(input: &str) -> String {
     todo!();
+}
+
+fn combine_first_and_last_digit(line_digits: &[u32]) -> u32 {
+    let first = line_digits.first().expect("No first digit");
+    let last = line_digits.last().expect("No last digit");
+
+    first * 10 + last
 }
 
 use crate::solution::Solution;
