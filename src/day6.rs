688199d075d009e9541ac66fc59416bc7b8a5073
Mårten Blankfors <marten@blankfors.se>
2023-12-06 11:04:02 +0100

    feat: Day 6 part 1 solved

--- src/day6.rs
+++ src/day6.rs
@@ -1,9 +1,28 @@
-pub fn p1(_input: &str) -> String {
-    todo!();
+pub fn p1(input: &str) -> String {
+    let mut it = input.trim().lines().map(|line| {
+        line.split_whitespace()
+            .skip(1)
+            .map(|s| s.parse().unwrap())
+            .collect::<Vec<i32>>()
+    });
+
+    let n: usize = it
+        .next()
+        .unwrap()
+        .into_iter()
+        .zip(it.next().unwrap())
+        .map(number_of_ways_to_win)
+        .product();
+
+    format!("Number of ways to win: {:?}", n)
 }
 
 pub fn p2(_input: &str) -> String {
     todo!();
+}
+
+fn number_of_ways_to_win((time, record): (i32, i32)) -> usize {
+    (0..time).filter(|x| x * (time - x) > record).count()
 }
 
 use crate::solution::Solution;
