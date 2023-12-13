6c4c05e874d151001aa712487196363c164d9f06
Mårten Blankfors <marten@blankfors.se>
2023-12-13 08:29:33 +0100

    feat: Day 13 part 1

--- src/day13.rs
+++ src/day13.rs
@@ -1,11 +1,109 @@
-pub fn p1(_input: &str) -> String {
-    todo!();
+pub fn p1(input: &str) -> String {
+    let patterns: Vec<Pattern> = input
+        .trim()
+        .split("\n\n")
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    let sum: i64 = patterns
+        .into_iter()
+        .map(|pattern| pattern.summarize_p1())
+        .sum();
+
+    format!("Sum: {}", sum)
 }
 
 pub fn p2(_input: &str) -> String {
     todo!();
 }
 
-use crate::solution::Solution;
+struct Pattern {
+    grid: HashMap<Pos, char>,
+    width: usize,
+    height: usize,
+}
+
+impl Pattern {
+    fn summarize_p1(&self) -> i64 {
+        self.find_vertical_reflection().unwrap_or_else(|| {
+            (self.height as i64 - self.find_horizontal_reflection().expect("Oh no!")) * 100
+        })
+    }
+
+    fn find_horizontal_reflection(&self) -> Option<i64> {
+        for y in 1..self.height as i64 {
+            let delta_lim = (self.height as i64 - y).min(y);
+
+            if (0..delta_lim).all(move |delta| {
+                let left = self.get_row(y - delta - 1);
+                let right = self.get_row(y + delta);
+
+                left == right
+            }) {
+                return Some(y);
+            };
+        }
+
+        None
+    }
+
+    fn find_vertical_reflection(&self) -> Option<i64> {
+        self.transpose().find_horizontal_reflection()
+    }
+
+    fn transpose(&self) -> Self {
+        let grid = self
+            .grid
+            .iter()
+            .map(|(pos, char)| (Pos::new(pos.im, pos.re), *char))
+            .collect();
+
+        let width = self.height;
+        let height = self.width;
+
+        Self {
+            grid,
+            width,
+            height,
+        }
+    }
+
+    fn get_row(&self, y: i64) -> Vec<char> {
+        (0..self.width as i64)
+            .map(|x| *self.grid.get(&Pos::new(x, y)).expect("Impossibru"))
+            .collect()
+    }
+
+    fn get_col(&self, x: i64) -> Vec<char> {
+        (0..self.height as i64)
+            .map(|y| *self.grid.get(&Pos::new(x, y)).expect("Impossibru"))
+            .collect()
+    }
+}
+
+impl FromStr for Pattern {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+
+        let (width, height) = parse::grid_dimensions(s);
+        let grid = parse::char_grid_iter(s)
+            .map(|(x, y, c)| (Pos::new(x as i64, y as i64), c))
+            .collect();
+
+        Ok(Self {
+            grid,
+            width,
+            height,
+        })
+    }
+}
+
+type Pos = num::Complex<i64>;
+
+use std::{collections::HashMap, str::FromStr};
+
+use crate::{parse, solution::Solution};
 inventory::submit!(Solution::new(13, 1, p1));
 inventory::submit!(Solution::new(13, 2, p2));
--- src/parse.rs
+++ src/parse.rs
@@ -13,3 +13,10 @@ pub fn char_grid_iter<T: TryFrom<char>>(
                 .filter_map(move |(x, c)| c.try_into().ok().map(|item| (x, y, item)))
         })
 }
+
+pub fn grid_dimensions(input: &str) -> (usize, usize) {
+    (
+        input.trim().lines().next().unwrap().trim().len(),
+        input.trim().lines().count(),
+    )
+}
