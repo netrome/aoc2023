353dcf7a1e2af52a098ff1f3272cbac6a5c7e9b3
Mårten Blankfors <marten@blankfors.se>
2023-12-15 06:42:29 +0100

    feat: Day 15 part 1 solved

--- src/day15.rs
+++ src/day15.rs
@@ -1,11 +1,29 @@
-pub fn p1(_input: &str) -> String {
-    todo!();
+pub fn p1(input: &str) -> String {
+    let s: u32 = input.trim().split(",").map(|s| hash(s.chars())).sum();
+    format!("Sum: {}", s)
 }
 
 pub fn p2(_input: &str) -> String {
     todo!();
 }
 
+fn hash(chars: impl IntoIterator<Item = char>) -> u32 {
+    chars
+        .into_iter()
+        .map(|c| c as u32)
+        .fold(0, |acc, v| ((acc + v) * 17) % 256)
+}
+
 use crate::solution::Solution;
 inventory::submit!(Solution::new(15, 1, p1));
 inventory::submit!(Solution::new(15, 2, p2));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hashing_works() {
+        assert_eq!(hash("HASH".chars()), 52)
+    }
+}
