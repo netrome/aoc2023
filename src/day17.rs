4ae287f7f1c63cb233e5b760dec963439ba38701
Mårten Blankfors <marten@blankfors.se>
2023-12-17 11:58:33 +0100

    feat: Day 17 part 2 solved

--- src/day17.rs
+++ src/day17.rs
@@ -2,8 +2,8 @@ pub fn p1(input: &str) -> String {
     solve::<BasicCrucible>(input)
 }
 
-pub fn p2(_input: &str) -> String {
-    todo!();
+pub fn p2(input: &str) -> String {
+    solve::<UltraCrucible>(input)
 }
 
 fn solve<C: Crucible>(input: &str) -> String {
@@ -42,13 +42,19 @@ fn solve<C: Crucible>(input: &str) -> String {
         }
     }
 
-    let end1 = C::new(factory(height), EAST);
-    let end2 = C::new(factory(width), SOUTH);
+    let d: u32 = distances
+        .into_iter()
+        .filter_map(|(crucible, distance)| {
+            if crucible.can_stop() && crucible.pos() == &factory(width) {
+                Some(distance)
+            } else {
+                None
+            }
+        })
+        .min()
+        .unwrap();
 
-    let d1 = distances.get(&end1).unwrap_or(&u32::MAX);
-    let d2 = distances.get(&end2).unwrap_or(&u32::MAX);
-
-    format!("Heat loss: {}", d1.min(d2))
+    format!("Heat loss: {}", d)
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -58,10 +64,11 @@ struct BasicCrucible {
     line_len: usize,
 }
 
-trait Crucible: Sized + Ord + Clone + std::hash::Hash {
+trait Crucible: Sized + Ord + Clone + std::hash::Hash + std::fmt::Debug {
     fn new(pos: Pos, dir: Dir) -> Self;
     fn advance(self, width: i64, height: i64) -> [Option<Self>; 3];
     fn pos(&self) -> &Pos;
+    fn can_stop(&self) -> bool;
 }
 
 impl Crucible for BasicCrucible {
@@ -108,6 +115,10 @@ impl Crucible for BasicCrucible {
     fn pos(&self) -> &Pos {
         &self.pos
     }
+
+    fn can_stop(&self) -> bool {
+        true
+    }
 }
 
 impl Ord for BasicCrucible {
@@ -117,6 +128,75 @@ impl Ord for BasicCrucible {
 }
 
 impl PartialOrd for BasicCrucible {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct UltraCrucible {
+    pos: Pos,
+    dir: Dir,
+    line_len: usize,
+}
+
+impl Crucible for UltraCrucible {
+    fn new(pos: Pos, dir: Dir) -> Self {
+        Self {
+            pos,
+            dir,
+            line_len: 0,
+        }
+    }
+
+    fn advance(self, width: i64, height: i64) -> [Option<Self>; 3] {
+        let left_dir = self.dir * NORTH;
+        let right_dir = self.dir * SOUTH;
+
+        let left = Self::new(self.pos + left_dir, left_dir);
+        let right = Self::new(self.pos + right_dir, right_dir);
+
+        let left = if within_grid(left.pos, height, width) && self.can_stop() {
+            Some(left)
+        } else {
+            None
+        };
+
+        let right = if within_grid(right.pos, height, width) && self.can_stop() {
+            Some(right)
+        } else {
+            None
+        };
+
+        let mut forward = self;
+        forward.pos += forward.dir;
+        forward.line_len += 1;
+
+        let forward = if within_grid(forward.pos, height, width) && forward.line_len < 10 {
+            Some(forward)
+        } else {
+            None
+        };
+
+        [forward, left, right]
+    }
+
+    fn pos(&self) -> &Pos {
+        &self.pos
+    }
+
+    fn can_stop(&self) -> bool {
+        self.line_len >= 3
+    }
+}
+
+impl Ord for UltraCrucible {
+    fn cmp(&self, _: &Self) -> std::cmp::Ordering {
+        std::cmp::Ordering::Equal
+    }
+}
+
+impl PartialOrd for UltraCrucible {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
@@ -142,7 +222,6 @@ const fn factory(width: i64) -> Pos {
 static NORTH: Dir = Dir::new(0, 1);
 static SOUTH: Dir = Dir::new(0, -1);
 static EAST: Dir = Dir::new(1, 0);
-static WEST: Dir = Dir::new(-1, 0);
 
 use std::{
     cmp::Reverse,
