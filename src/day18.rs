d2eb6ae8ab6db137981c9561a2f6dea3a1aacfc7
Mårten Blankfors <marten@blankfors.se>
2023-12-18 11:33:12 +0100

    feat: Day 18 part 1 solved

--- src/day18.rs
+++ src/day18.rs
@@ -1,10 +1,184 @@
-pub fn p1(_input: &str) -> String {
-    todo!();
+pub fn p1(input: &str) -> String {
+    let plan: DigPlan = input.parse().unwrap();
+    let trench = plan.loop_of_trench();
+    let trench_set = trench.iter().cloned().collect();
+
+    let dim = Dim::infer(trench);
+    let dim = dim.expand();
+    let outside = fill(dim.bottom_left, &dim, &trench_set);
+
+    let lagoon_area = dim.area() as usize - outside.len();
+
+    format!("Interior volume: {}", lagoon_area)
 }
 
 pub fn p2(_input: &str) -> String {
     todo!();
 }
+
+fn fill(point: Pos, dim: &Dim, border: &HashSet<Pos>) -> HashSet<Pos> {
+    let mut q = vec![point];
+    let mut res = HashSet::new();
+    res.insert(point);
+
+    while let Some(pos) = q.pop() {
+        for pos2 in neighbors(&pos) {
+            if dim.contains(pos2) && !border.contains(&pos2) && !res.contains(&pos2) {
+                res.insert(pos2);
+                q.push(pos2);
+            }
+        }
+    }
+
+    res
+}
+
+#[derive(Debug)]
+struct DigPlan {
+    instrs: Vec<Instr>,
+}
+
+impl DigPlan {
+    fn loop_of_trench(&self) -> Vec<Pos> {
+        self.instrs
+            .iter()
+            .fold(vec![Pos::new(0, 0)], |mut acc, instr| {
+                let last_pos = *acc.last().expect("Impossibru");
+                let dir = instr.dir.from_above();
+
+                (0..instr.len as i64).for_each(|offset| acc.push(last_pos + (offset + 1) * dir));
+                acc
+            })
+    }
+}
+
+impl FromStr for DigPlan {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let instrs = s
+            .trim()
+            .lines()
+            .map(|line| sscanf::scanf!(line.trim(), "{Instr}").unwrap())
+            .collect();
+
+        Ok(Self { instrs })
+    }
+}
+
+#[derive(Debug, sscanf::FromScanf)]
+#[sscanf(format = "{dir} {len} ({color})")]
+struct Instr {
+    dir: Direction,
+    len: usize,
+    color: Color,
+}
+
+#[derive(Debug, Clone, Copy, sscanf::FromScanf)]
+enum Direction {
+    #[sscanf(format = "U")]
+    Up,
+    #[sscanf(format = "D")]
+    Down,
+    #[sscanf(format = "L")]
+    Left,
+    #[sscanf(format = "R")]
+    Right,
+}
+
+impl Direction {
+    fn from_above(&self) -> Dir {
+        match self {
+            Self::Up => NORTH,
+            Self::Down => SOUTH,
+            Self::Left => WEST,
+            Self::Right => EAST,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, sscanf::FromScanf)]
+#[sscanf(format = "#{r:x}{g:x}{b:x}")]
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+#[derive(Debug, Clone)]
+struct Dim {
+    bottom_left: Pos,
+    top_right: Pos,
+}
+
+impl Dim {
+    fn infer(positions: impl IntoIterator<Item = Pos>) -> Self {
+        let mut it = positions.into_iter();
+        let init = it.next().unwrap();
+
+        it.fold(
+            Self {
+                bottom_left: init,
+                top_right: init,
+            },
+            |acc, pos| acc.extend_to_fit(pos),
+        )
+    }
+
+    fn extend_to_fit(mut self, pos: Pos) -> Self {
+        self.bottom_left.im = self.bottom_left.im.min(pos.im);
+        self.bottom_left.re = self.bottom_left.re.min(pos.re);
+
+        self.top_right.im = self.top_right.im.max(pos.im);
+        self.top_right.re = self.top_right.re.max(pos.re);
+
+        self
+    }
+
+    fn contains(&self, pos: Pos) -> bool {
+        self.bottom_left.im <= pos.im
+            && pos.im <= self.top_right.im
+            && self.bottom_left.re <= pos.re
+            && pos.re <= self.top_right.re
+    }
+
+    fn expand(&self) -> Self {
+        Self {
+            bottom_left: self.bottom_left + Pos::new(-1, -1),
+            top_right: self.top_right + Pos::new(1, 1),
+        }
+    }
+
+    fn area(&self) -> i64 {
+        let diff = (self.top_right - self.bottom_left);
+        (diff.re + 1) * (diff.im + 1)
+    }
+}
+
+fn neighbors(point: &Pos) -> [Pos; 8] {
+    [
+        point + NORTH,
+        point + NORTH + EAST,
+        point + NORTH + WEST,
+        point + EAST,
+        point + WEST,
+        point + SOUTH,
+        point + SOUTH + EAST,
+        point + SOUTH + WEST,
+    ]
+}
+
+type Pos = Complex<i64>;
+type Dir = Complex<i64>;
+
+static NORTH: Dir = Dir::new(0, 1);
+static SOUTH: Dir = Dir::new(0, -1);
+static EAST: Dir = Dir::new(1, 0);
+static WEST: Dir = Dir::new(-1, 0);
+
+use std::{collections::HashSet, str::FromStr};
+
+use num::Complex;
 
 use crate::solution::Solution;
 inventory::submit!(Solution::new(18, 1, p1));
